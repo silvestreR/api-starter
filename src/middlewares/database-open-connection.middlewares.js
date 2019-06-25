8361f5740aeadb11c5f67ed6ab74a171b6b97b27
Renato Silvestre (SilvestreHH) <renatao.silvestre@gmail.com>
2019-06-25 11:34:10 -0300

    init

--- src/middlewares/database-open-connection.middlewares.js
+++ src/middlewares/database-open-connection.middlewares.js
@@ -4,7 +4,11 @@ const start = ( uri ) => {
   const connectionError = ( err ) =>
     console.log(`An error has occurred when trying to start the MongoDB Database for URI:\n${ uri }`, err )
 
-  const conn = Mongoose.createConnection( uri, {  poolSize: 5 } )
+  const mongoConnectionOptions = {
+    poolSize: 5, useNewUrlParser: true
+  }
+
+  const conn = Mongoose.createConnection( uri, mongoConnectionOptions )
   conn.on('error', connectionError )
   return conn
 }
@@ -13,7 +17,7 @@ const databaseConnection = async ( req, res, next ) => {
 
   const connection = await start( req.$client.dbHost )
 
-  if ( ( connection instanceof Error ) || !connection ){
+  if ( ( connection instanceof Error ) || !connection ) {
     const errorObject = {
         clientError: req$client
       , message: connection.message || 'An error has occurred when trying to start the Database'
--- src/mongoDbStart.js
+++
@@ -1,7 +0,0 @@
-const MongoDB = require('./mongoDb.js')
-const MongoDBConnections = require('./mongoDbConnections.js')
-
-const startNewConnection = ( connection ) =>
-  MongoDB.start( connection, MongoDBConnections[ connection ] )
-
-module.exports = startNewConnection
