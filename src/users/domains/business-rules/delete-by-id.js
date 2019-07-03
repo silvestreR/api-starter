3e88c1763bf241fb0ebe33902bb00fc21af257c0
Renato Silvestre (SilvestreHH) <renatao.silvestre@gmail.com>
2019-07-03 10:17:43 -0300

    inclusao do metodo get-all e remove no delete-by-id

--- src/users/domains/business-rules/delete-by-id.js
+++ src/users/domains/business-rules/delete-by-id.js
@@ -5,6 +5,6 @@ const deleteById = async ( req, query ) => {
   const foundUser = await model.findOne( query )
   if (!foundUser)
     throw new Error('userError: User Not Found.')
-  return model.update( query, {status: false}, {multi: false} )
+  return model.remove( query )
 }
 module.exports = { deleteById }
